297ddf33699d9e091e3f2b9cb3b0a702e7fefef0
Maxime Turcotte <maxime.turcotte@erudit.org>
2019-06-25 15:06:15 -0400

    Replace active field on AccountActionToken model by is_canceled
    ref eruditorg#2254

--- account_actions/admin.py
+++ account_actions/admin.py
@@ -7,10 +7,10 @@ from .models import AccountActionToken
 
 class AccountActionTokenAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'key', 'email', 'first_name', 'last_name', 'action', 'active', 'expiration_date',
+        'id', 'key', 'email', 'first_name', 'last_name', 'action', 'is_canceled', 'expiration_date',
         'is_expired', 'is_consumed', )
     list_display_links = ('id', 'key', 'email', )
-    list_filter = ('action', 'active', )
+    list_filter = ('action', 'is_canceled', )
     search_fields = ('email', 'first_name', 'last_name', )
 
 
