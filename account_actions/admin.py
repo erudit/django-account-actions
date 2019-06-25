5793e71eb09b867f7a2524a2db35087aa19a74a6
Maxime Turcotte <maxime.turcotte@erudit.org>
2019-06-25 15:17:26 -0400

    Add actions to cancel/uncancel tokens in batch
    ref eruditorg#2254

--- account_actions/admin.py
+++ account_actions/admin.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
 
 from .models import AccountActionToken
 
@@ -12,6 +14,15 @@ class AccountActionTokenAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'key', 'email', )
     list_filter = ('action', 'is_canceled', )
     search_fields = ('email', 'first_name', 'last_name', )
+    actions = ('cancel', 'uncancel', )
+
+    def cancel(self, request, queryset):
+        queryset.update(is_canceled=True)
+    cancel.short_description = _('Cancel selected %(verbose_name_plural)s')
+
+    def uncancel(self, request, queryset):
+        queryset.update(is_canceled=False)
+    uncancel.short_description = _('Uncancel selected %(verbose_name_plural)s')
 
 
 admin.site.register(AccountActionToken, AccountActionTokenAdmin)
